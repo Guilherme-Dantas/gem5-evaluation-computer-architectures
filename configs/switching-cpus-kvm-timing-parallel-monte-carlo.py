0be0586e2caa0356c6d208c365f105cab1c0abf2
Guilherme Dantas <72616807+Guilherme-Dantas@users.noreply.github.com>
2023-07-28 01:34:40 -0300

    Inclusão de nova config

--- configs/switching-cpus-kvm-timing-parallel-monte-carlo.py
+++ configs/switching-cpus-kvm-timing-parallel-monte-carlo.py
@@ -9,12 +9,9 @@ from gem5.isas import ISA
 from gem5.coherence_protocol import CoherenceProtocol
 from gem5.simulate.simulator import Simulator
 from gem5.simulate.exit_event import ExitEvent
-from gem5.resources.workload import Workload
 from gem5.resources.resource import DiskImageResource
 from gem5.resources.resource import Resource
 
-# This runs a check to ensure the gem5 binary is compiled to X86 and to the
-# MESI Two Level coherence protocol.
 requires(
     isa_required=ISA.X86,
     coherence_protocol_required=CoherenceProtocol.MESI_TWO_LEVEL,
@@ -25,7 +22,6 @@ from gem5.components.cachehierarchies.ruby.mesi_two_level_cache_hierarchy import
     MESITwoLevelCacheHierarchy,
 )
 
-# Here we setup a MESI Two Level Cache Hierarchy.
 cache_hierarchy = MESITwoLevelCacheHierarchy(
     l1d_size="16kB",
     l1d_assoc=8,
@@ -36,15 +32,8 @@ cache_hierarchy = MESITwoLevelCacheHierarchy(
     num_l2_banks=1,
 )
 
-# Setup the system memory.
 memory = SingleChannelDDR3_1600(size="3GB")
 
-# Here we setup the processor. This is a special switchable processor in which
-# a starting core type and a switch core type must be specified. Once a
-# configuration is instantiated a user may call `processor.switch()` to switch
-# from the starting core types to the switch core types. In this simulation
-# we start with KVM cores to simulate the OS boot, then switch to the Timing
-# cores for the command we wish to run after boot.
 processor = SimpleSwitchableProcessor(
     starting_core_type=CPUTypes.KVM,
     switch_core_type=CPUTypes.TIMING,
@@ -52,7 +41,6 @@ processor = SimpleSwitchableProcessor(
     num_cores=2,
 )
 
-# Here we setup the board. The X86Board allows for Full-System X86 simulations.
 board = X86Board(
     clk_freq="3GHz",
     processor=processor,
@@ -60,16 +48,6 @@ board = X86Board(
     cache_hierarchy=cache_hierarchy,
 )
 
-# Here we set the Full System workload.
-# The `set_kernel_disk_workload` function for the X86Board takes a kernel, a
-# disk image, and, optionally, a command to run.
-
-# This is the command to run after the system has booted. The first `m5 exit`
-# will stop the simulation so we can switch the CPU cores from KVM to timing
-# and continue the simulation to run the echo command, sleep for a second,
-# then, again, call `m5 exit` to terminate the simulation. After simulation
-# has ended you may inspect `m5out/system.pc.com_1.device` to see the echo
-# output.
 command = "m5 exit;" \
         + "echo 'Executing monte carlo parallel.';" \
         + "cd ../home/gem5/;" \
@@ -88,10 +66,6 @@ board.set_kernel_disk_workload(
 simulator = Simulator(
     board=board,
     on_exit_event={
-        # Here we want override the default behavior for the first m5 exit
-        # exit event. Instead of exiting the simulator, we just want to
-        # switch the processor. The 2nd m5 exit after will revert to using
-        # default behavior where the simulator run will exit.
         ExitEvent.EXIT: (func() for func in [processor.switch])
     },
 )

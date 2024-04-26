ddef669bbf1017bf3f1cc3dbcdee231753287ec3
Guilherme Dantas <72616807+Guilherme-Dantas@users.noreply.github.com>
2024-04-26 15:50:16 -0300

    Improving workload

---
+++ payloads/pi-estimation/monte-carlo-parallel-1000.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cmath>
+#include <random>
+#include <omp.h>
+#include <chrono>
+
+using namespace std;
+using std::chrono::duration;
+using std::chrono::duration_cast;
+using std::chrono::high_resolution_clock;
+using std::chrono::milliseconds;
+
+int main()
+{
+    long long int dots_quantity = 1000;
+
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<> distribution_range(-1.0, 1.0);
+
+    long long int dots_in_circle = 0;
+
+    auto start = high_resolution_clock::now();
+
+    #pragma omp parallel for reduction(+ : dots_in_circle)
+    for (int i = 0; i < dots_quantity; i++)
+    {
+        double x = distribution_range(gen);
+        double y = distribution_range(gen);
+        if (sqrt(x * x + y * y) <= 1.0)
+        {
+            dots_in_circle++;
+        }
+    }
+
+    std::cout << "Total dots in circle: " << dots_in_circle << endl;
+
+    double pi = 4.0 * dots_in_circle / dots_quantity;
+
+    auto end = high_resolution_clock::now();
+    auto execution_time_ms = duration_cast<milliseconds>(end - start);
+
+    std::cout << "Estimated pi value: " << pi << " in " << execution_time_ms.count() << "ms" << endl;
+    return 0;
+}
